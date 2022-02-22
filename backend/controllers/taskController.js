fb1729de6be3bd41385fd8d97aec0bd24c06c4a7
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2022-02-22 16:35:41 -0500

    Documentacion necesaria en taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,9 +3,17 @@ import task from "../models/task.js";
 import user from "../models/user.js";
 
 
-
+/**
+ * Registro de tarea
+ * @param {*} req  Se requiere el id del usuario previamente registrado en DB jutno co los demas datos
+ * necesarios para el registro.
+ * @param {*} res 
+ * @returns 
+ */
 const registerTask = async (req, res) => {
     const{userId,name,description,url} = req.body;
+    if(!userId || !name || !description || !url)
+        return res.status(400).send({message:"Incomplete Data"});
     const tasksSchema = new task({
         user:userId,
         name:name,
@@ -23,6 +31,12 @@ const registerTask = async (req, res) => {
     
 }
 
+/**
+ * Listar tareas
+ * @param {*} req se requiere el id del usuario al que se desea consultar sus tareas, este debe ir en el body de la peticion, en el parametro por url se indica el estado por el que se desea que las tareas sean listadas (to-do, in-progress y finish).
+ * @param {*} res 
+ * @returns 
+ */
 const listTasks = async (req, res) => {
     const{userId}=req.body;
     const{status}=req.params;
@@ -37,6 +51,12 @@ const listTasks = async (req, res) => {
      
 };
 
+/**
+ * eliminar tareas
+ * @param {*} req se requiere el id de la tarea que se va a eliminar
+ * @param {*} res 
+ * @returns 
+ */
 const deleteTask = async (req, res) => {
     const{_id}=req.params;
     const taskDeleted = await task.findByIdAndDelete(_id);
@@ -47,6 +67,14 @@ const deleteTask = async (req, res) => {
 
 };
 
+/**
+ * Listar tareas
+ * @param {*} req se requiere el id de la tarea a actualizar junto a los datos que pueden ser editados
+ * (taskStatus e imageUrl). el taskStatus debe coincidir con los estados permitidos.
+ * ("to-do","in-progress","finish")
+ * @param {*} res 
+ * @returns 
+ */
 const updateTask = async (req, res) => {
     const{_id,taskStatus,imageUrl}=req.body;
     const status =["to-do","in-progress","finish"];
@@ -62,7 +90,5 @@ const updateTask = async (req, res) => {
      : res.status(200).send({ message: "Success Update" });
 
 }
-
-//const registerUser = async (req, res) => {}
 
 export default {deleteTask,listTasks,registerTask,updateTask};
